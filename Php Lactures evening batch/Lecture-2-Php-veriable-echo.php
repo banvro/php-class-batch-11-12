3bb14eefd160aa3fbefc02d1743432fcda460951
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-09-10 17:20:36 +0530

    Add files via upload

---
+++ Php Lactures evening batch/Lecture-2-Php-veriable-echo.php
@@ -0,0 +1,233 @@
+<?php
+
+// Veriables: 
+    // are containers helps us to store data temp.
+
+// $x = 10;
+
+// rules----> help too create veriable
+
+// 1) veriable always start with $.
+// 2) Never hold spaces or spaciacl symbols.
+// $full name = "Python";
+
+// $fullname = "Modulo";
+
+// $full_name = "Hello wolrd";
+
+
+// 3) Veriable never start with space or any digit.
+// 4) it always start with char or underscore
+
+
+// $x = 20;
+
+// $1x = 100;
+
+// $xyz = 20000;
+
+// $x1 = 10;
+
+
+// 5) Veriable never be a keyword.
+
+
+// keywords ---> are those words which are reserved by php for spacial purpose.
+
+
+// class
+
+
+// $class = 10;
+
+// $echo = 20;
+
+// echo, class, function, if, else, else if, for, foreach.....
+
+
+
+// ------------------------------
+
+// echo   |    print   |  print_r
+
+// echo ---> construct
+
+// echo 
+//     --> to show ouptut...on browser..
+    
+//     text, numbers, html, 
+
+
+// echo "Hellow php!";
+
+// echo 1000;
+
+// echo $hey;
+
+
+// $full_name = "Hllow wolrd";
+
+// echo $full_name;
+
+
+// $x = "hello";
+
+// $y = "world";
+
+// // echo $x, $y;
+
+// echo $x, " ", $y;
+
+
+// Problem : Adding two numbers
+
+
+// $x = 1000;
+
+// $y = 2394;
+
+// $z = $x + $y;
+
+// // echo "The sum of ", $x, " and ", $y, " is : ", $z;
+
+// echo "The sum of $x and $y is : $z";
+
+
+
+// Concatenation
+//     ---> joiing two strings
+
+
+// $zx = "Hello";
+// $pq = "World";
+
+// echo $zx . " ". $pq;
+
+
+// $x = "Python";
+
+// echo '$x is a programming language';
+
+
+// echo "
+//     hello
+//     python
+
+// ";
+
+
+// echo 'eeeeeee';
+
+// echo "helooooo";
+
+
+// Escape Charcters
+
+
+// \n ---> for new line
+
+// echo "helloww";
+
+// echo "heyyyyy";
+
+
+// echo "Python is a \nprogaramming lanague..\n and php is use for\n web devlopment";
+
+
+// \t
+
+// echo "hellow\tworld!";
+
+
+
+
+// veriables
+    // ----> veriable case senstive
+    
+// $Name = "Kriss";
+
+// echo $Name;
+
+
+
+
+
+// print ----> echo
+
+
+
+// $x = 1000;
+
+// $y = 2000;
+
+// echo $x, $y;
+
+// echo --> multiple values show --> using commas.
+
+// 2) echo powerfull ---> do not return anything
+
+
+// ------------------------------------
+
+// print ---> help us to show single value
+
+// print $x, $y;
+
+// $x = "hellooo";
+// $y = "okkkkk";
+
+// $k = print $x . $y ."\n";
+
+// echo $k;
+
+// echo  --- print
+
+// print_r() ----> function ---> array, objects
+
+
+// $arr = array("applle", "banana", "manago", "orange");
+
+// print_r($arr);
+
+
+
+
+
+
+// Comments in php: Team
+
+
+//         1 Project
+    
+//     10
+    
+//      ----> code --> 500 lines
+
+// ----> helps to increse code readablity
+
+// Types of comments:
+//     1) Single line comment
+//     2) multi line comment
+
+
+// 1) Single line comment:
+//     1) // 
+//     2) #
+
+// heyyyyyyyyyyyyyyyyy
+
+# nooooooooooooooooooo
+
+
+// 2) multi line comment:
+
+/*
+    aksdbajsbdkabdksad
+    aashdbasbdsadkasbdjkabsd
+    asdbakdbkajkdbas
+    aldnasdn
+*/
+
+
+
+?>
