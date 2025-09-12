ca59eabd225d86a588ba72af9763e7e63d69f6fc
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-09-12 17:25:31 +0530

    Create Lecture-4-operators.php

---
+++ Php Lactures evening batch/Lecture-4-operators.php
@@ -0,0 +1,232 @@
+<?php
+
+// Operators
+
+// $x = 20;
+
+// PHP ----> 8 Operators
+
+
+// 1) Airthmatical Operator:
+//     1) +
+//     2) -
+//     3) *
+//     4) /
+//     5) % 
+//     6) **
+
+// echo 4 / 2;
+
+// echo 10 % 2;
+
+
+// echo 3 ** 4
+
+
+// 2) Assignment Operators:
+    // 1) =
+    // 2) +=
+    // 3) -=
+    // 4) *=
+    // 5) /=
+    // 6) %=
+
+
+// $name = "PHP";
+
+// $x = 20;
+
+// $y = $x + 10;
+
+// echo $y;
+
+ // 2) +=
+ 
+// echo $x += 10 # $x = $x + 10;
+
+
+// $q = 10;
+
+// $q -= 3;
+
+// echo $q;
+
+
+
+// 3) Comparison Operators:
+
+    // 1) ==
+    // 2) ===
+    // 3) !=
+    // 4) >
+    // 5) <
+    // 6) >=
+    // 7) <=
+    // 8) Spaceship <=>
+
+
+// ---> output (True   |   False)
+
+
+// echo 11 == 10;
+
+// echo "Hello" == "hello";
+
+// echo "10" == 10;
+
+
+//  2) === (Identical Operator)
+
+// echo "10" === 10;
+
+
+// 3) !=  (not equal to operator)
+
+
+// echo 10 != 10;
+
+// echo 1000 != 10;
+
+// echo "10" != 10;
+
+// echo "10" !== 10;
+
+
+// 4) >
+
+// echo 10 > 19;
+
+// echo 1 > 0;
+
+
+// echo 100 > 99;
+
+// echo 10 < 90;
+
+// echo 10 <= 10;
+
+
+
+
+
+
+
+
+
+// 8) Spaceship <=>
+
+// if left < right ------> -1
+// if left == right -----> 0
+// if left > right ------> 1
+
+// echo 3 <=> 3;
+// echo "\n";
+
+// echo 5 <=> 10;
+// echo "\n";
+
+// echo 100 <=> 3;
+
+
+
+
+
+// 4) Increment / Decrement operators:
+    // 1) ++$x
+    // 2) $x++
+    // 3) --$x;
+    // 4) $x--
+
+
+// 1) ++$x (Pre-increment Operator):
+
+// $q = 5;
+
+// echo ++$q;
+
+// // 2) 2) $x++ (POST increment operator):
+
+// echo $q;
+
+// echo $q++;
+
+// echo $q;
+
+
+// 3) --$x;
+
+// $x = 4;
+
+// echo $x . "\n";
+
+// --$x;
+
+// echo $x . "\n";
+
+
+
+
+// 5) Logical Operators:
+
+// 1) && / and
+// 2) xor  ----> or operator
+// 3) !  ---> not operaor
+
+
+// and 
+
+// 0     0     ---> 0
+// 1     0     ---> 0
+// 0     1     ---> 0
+// 1     1     ---> 1
+
+
+// $age = 20;
+
+// echo $age > 10 and $age == 21;
+
+
+
+// xor
+
+// 0    0   ------> 0
+// 1    0    -----> 1
+// 0    1 ------->  1
+// 1    1  ------>  1
+
+
+// $age = 20;
+
+// echo $age > 10 xor $age == 21;
+
+
+
+
+// 3) !  ---> not operaor
+
+// $x = 10;
+
+// echo !($x > 4);
+
+
+
+
+// ----------------------------
+
+
+// Ternary OPerator ?
+
+
+// $age = 2;
+
+// echo ($age >= 18) ? "Adult" : "Child";
+
+
+
+
+
+
+
+
+
+?>
