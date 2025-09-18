6101c1ac6bb44fc9fbe498604d631ec0f1044a2e
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-09-18 17:07:30 +0530

    Create Lecture-6-for-while-loop.php

---
+++ Php Lactures evening batch/Lecture-6-for-while-loop.php
@@ -0,0 +1,110 @@
+<?php
+
+
+// for loop
+//     ----> ittrate data
+
+// for (variable init, condation,icrementdecrement){
+//     block of code
+// }
+
+
+
+// for ($a = 0; $a < 11; $a++){
+//     echo "$a"."\n";
+
+// }
+
+
+
+// 2 x 1 = 2
+// 2 x 2 = 4
+// 2 x 3 = 6
+// .
+// ..
+// .
+// .
+// .
+
+// 2 x 10 = 20
+
+
+
+// for ($i = 0; $i <= 100; $i++){
+//     if ($i % 3 == 0){
+//         echo $i . "\n";
+//     }
+// }
+
+
+// 0 ----> 100
+
+// mulitple of 3 ----> fizz
+// multiple of 5 ---> buzz
+// multiple of 3 and 5 ----> fizzbuzz
+
+// 0
+// 1
+// 2
+// fizz
+// 4
+// buzz
+// fizz
+// 7
+// 8
+// fizz
+// buzz
+// 11
+// fizz
+// 13
+// 14
+// fizzbuzz
+// 1
+
+
+// while loop
+
+// $x = 10;
+
+// while ($x > 1){
+//     echo "$x \n";
+//     $x--;
+// }
+
+
+// $x = 0;
+
+// do{
+//     echo $x."\n";
+//     $x++;
+// }while ($x == 11)
+
+
+
+// $x = 500;
+
+// // subract --> 120
+//     ---> 
+
+
+$x = 500;
+$counter = 0;
+
+while ($x > 0){
+    $counter++;
+    $x -= 120;
+    
+    echo "ittration $counter --> data $x \n";
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
