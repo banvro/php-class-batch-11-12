50d27efdce21fc67ae5f4496dc04d4dd8963204c
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-09-19 17:24:44 +0530

    Create php-start-array-basic.php

---
+++ Php Lactures evening batch/php-start-array-basic.php
@@ -0,0 +1,86 @@
+<?php
+
+// for ---> while loop
+//     ----> foreach()
+
+// Array :-> PHP
+
+    // Types of arryas:
+    //         1) Indexed Array
+    //         2) Assocoative Arrays
+    //         3) MultiDimensional Array
+
+
+// 1) Indexed Array:
+
+// $arr = ["Hey", "Hello", "Ok", "Done"];
+
+// print_r($arr);
+    
+    
+// $x = array("nooo", "hloo", "ok");
+    
+// print_r($x);
+    
+// echo "\n";
+
+// var_dump($x);
+
+// echo "\n";
+
+// var_export($x);
+
+// echo "\n";
+
+// implode ---> help to convert array to string
+
+// echo implode(", ", $x);
+
+// echo
+    
+    
+//  $x = array("nooo", "hloo", "ok");
+ 
+//  arrar ---> json
+
+// echo json_encode($x);
+
+// indexed array
+
+
+// $x = array("heyyy", "nooo", "okkk", "nicee");
+    
+// print_r($x);
+
+// echo $x[2];
+
+// echo count($x);
+
+
+// for ($i = 0; $i < count($x) ; $i++){
+//     echo $x[$i]." \n";
+// }
+
+
+// $x = "heyyyyyyyyyyy";
+
+// echo $x[4];
+
+
+
+
+
+$zx = "How are you!";
+
+
+$x = strlen($zx);
+
+
+for ($i = 0; $i < $x; $i++){
+    echo $zx[$i]. "\n";
+}
+
+    
+
+
+?>
