2f736c7aa05916bb2db235b3dfb8fee266cd1682
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-09-29 17:31:45 +0530

    Create statments-switch0brea-continue.php

---
+++ Php Lactures evening batch/statments-switch0brea-continue.php
@@ -0,0 +1,151 @@
+<?php
+
+// Control Statments:
+//     -->
+
+// for ($i = 0; $i < 11; $i++){
+//     echo $i . "\n";
+    
+//     if ($i == 7){
+//         break;
+//     }
+// }
+
+// continue
+    // --> skip
+
+
+
+
+// $x = [12, 1, 23, 1, 12, 23, 34, 45, 6, 7, 845, 23, 1, 23, 2, 4, 5, 6];
+
+// foreach ($x as $i){
+    
+//     if ($i < 20){
+//         continue;
+//     }
+    
+//     echo $i ."\n";
+// }
+
+
+// if (10 == 10){
+    
+// }
+
+
+
+
+
+// Switch ---> control structure
+//     --> help us to compare ---> verable ko multiple posibalities.
+    
+//     if elif else
+
+
+// Syntax
+
+// switch (expression){
+//     case value1:
+//         ------>
+//         break;
+//     case value 2:
+//          ----> 
+//          break
+//          ..
+         
+//          .
+         
+//     default:
+//         ---> 
+// }
+
+
+
+
+// Even Odd number
+
+// $x = 5;
+
+// switch ($x % 2){
+    
+//     case 0:
+//         echo "this is even number";
+//         break;
+    
+//     case 1:
+//         echo "This is odd number";
+//         break;
+// }
+
+
+// Voating system
+
+
+
+// $grade = "I";
+
+// switch ($grade){
+//     case "A":
+//         echo "Excelent";
+     
+//     case "B":
+//         echo "good";
+//         break;
+    
+//     case "c":
+//         echo "poor";
+//         break;
+    
+//     default:
+//         echo "work haerder";
+// }
+
+// $day = "Sunday";
+
+// switch ($day){
+    
+//     case "Saturday":
+//     case "Sunday":
+//         echo "Weekend!";
+//         // break;
+    
+//     default:
+//         echo "Weekdays";
+// }
+
+
+
+
+Qeustion:
+    
+
+zx = "This";
+
+sihT
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+?>
